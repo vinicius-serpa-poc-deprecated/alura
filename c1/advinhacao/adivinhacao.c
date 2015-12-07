bc1b452f8ac7472ffe82a298ddf2877f25e4d6b0
Vinicius Serpa <vinicius-serpa@live.com>
2015-12-07 17:26:25 -0200

    C I - Adivinhacao

---
+++ c1/advinhacao/adivinhacao.c
@@ -0,0 +1,59 @@
+/*
+ * adivinhacao.c
+ *
+ *  Created on: 7 de dez de 2015
+ *      Author: saocmest
+ *
+ *      gcc adivinhacao.c -o adivinhacao.exe
+ */
+
+// A diretiva include permite incluir bibliotecas
+#include <stdio.h>
+
+// A diretiva define permite definir uma constante
+#define TIMES_TO_PLAY 5
+
+int main() {
+
+	int secretNumber;
+	int userSelection;
+	int i;
+
+	// print the header game
+	printf("******************************************************\n");
+	printf("******************** Chute Certo! ********************\n");
+	printf("******************************************************\n");
+
+	secretNumber = 42;
+
+	for (i = 1; i <= TIMES_TO_PLAY; i++) {
+
+		printf("\n\nTentativa %d de %d!", i, TIMES_TO_PLAY);
+		printf("\nQual é o seu chute?");
+
+		fflush(stdout);
+		scanf("%d", &userSelection);
+
+		printf("\nO numero %d é o chute.", userSelection);
+
+		if (userSelection == secretNumber) {
+
+			printf("\nVocê acertou!");
+			break;
+
+		} else {
+
+			printf("\nNão foi dessa vez!");
+
+			if (userSelection > secretNumber)
+				printf("\nSeu chute foi maior que o número secreto!");
+			else
+				printf("\nSeu chute foi menor que o número secreto!");
+		}
+
+	}
+
+	printf("\nFim de Jogo!");
+
+	return 0;
+}
