b2e4e51c72d010b6134e7548c52d36490a3ef5c8
Vinicius Serpa <vinicius-serpa@live.com>
2016-01-11 20:08:07 -0200

    initial commit

---
+++ spring-mvc-1/casadocodigo/src/main/java/br/com/casadocodigo/loja/config/ServletSpringMvc.java
@@ -0,0 +1,23 @@
+package br.com.casadocodigo.loja.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {		
+		return new Class[] {AppWebConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {		
+		return new String[] {"/"};
+	}
+
+}
---
+++ spring-mvc-1/readme.txt
@@ -0,0 +1,4 @@
+jboss forge - framework para geração de projetos maven
+	- executar forge.bat em c:\program files (x86)\forgefolder\bin\
+	- ir até a pasta do projeto
+	- criar projeto: project-new --named [projectname]
